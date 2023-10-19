ecefb75970548ede3f898726ee572b87b9aefee4
Abdelrahman Mahmoud <108185949+abdelrahman0mahmoud@users.noreply.github.com>
2023-10-19 12:46:20 +0300

    Add Interns

--- js/interns.js
+++ js/interns.js
@@ -2,7 +2,6 @@
 function addStudentBorder(imgSrc, name, id, score, projectsNum, stu_level) {
     const border_stu = document.createElement("div");
     border_stu.classList.add("border");
-    border_stu.classList.add("border-0");
     border_stu.classList.add(stu_level);
     
     // Student image
@@ -14,7 +13,7 @@ function addStudentBorder(imgSrc, name, id, score, projectsNum, stu_level) {
     // Student name
     const p_name = document.createElement("p");
     p_name.classList.add("stu-name");
-    p_name.textContent = "Name: " + name;
+    p_name.textContent = name;
 
     // Student ID
     const p_id = document.createElement("p");
@@ -48,9 +47,52 @@ function addStudentBorder(imgSrc, name, id, score, projectsNum, stu_level) {
 
 //Golden Students 
 //Silver Students
-addStudentBorder("img/user.png", "User", 1,  "%", 15, "silver");
-addStudentBorder("img/user.png", "User", 2,  "%", 15, "silver");
-addStudentBorder("img/user.png", "User", 3,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Shrouk ragap abdelazim", 1,  "%", 15, "silver");
+addStudentBorder("img/user.png", "ميرنا سامح سيد محمد", 2,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Sama Hesham Sayed", 3,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Waleed Mohamed", 4,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Kholoud Hassan Tawfik", 5,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Hamza mohamed", 6,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Jana Mohamed Ibrahim ", 7,  "%", 15, "silver");
+addStudentBorder("img/user.png", "alaa mohamed abdellatif", 8,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Amal Farid Galal", 9,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Hagar Ahmed Elfakharany", 10,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Mina Khalifa", 11,  "%", 15, "silver");
+addStudentBorder("img/user.png", "مينا كمال بطرس", 12,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Ali Ahmed", 13,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Mazen Abd-El Hameed", 14,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Fatma khaled abdelwahab", 15,  "%", 15, "silver");
+addStudentBorder("img/user.png", "ميسرة محمد سيد العوام", 16,  "%", 15, "silver");
+addStudentBorder("img/user.png", "بسمة عبدالخالق", 17,  "%", 15, "silver");
+addStudentBorder("img/user.png", "مهاب محمد سيد", 18,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Mina Anis", 19,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Donia Sherif", 20,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Amira Ibrahim Mabrouk", 21,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Ezzeldien Abdelwahab", 22,  "%", 15, "silver");
+addStudentBorder("img/user.png", "محمد مجدي عبادة", 23,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Manar Khaled abdelgawad ", 24,  "%", 15, "silver");
+addStudentBorder("img/user.png", "nour adly mostafa", 25,  "%", 15, "silver");
+addStudentBorder("img/user.png", "يوسف عبدالحميد", 26,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Abdallah Mahmoud Fawzy", 27,  "%", 15, "silver");
+addStudentBorder("img/user.png", "ahmed khafagy", 28,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Manar Emad ElDeen ", 29,  "%", 15, "silver");
+addStudentBorder("img/user.png", "مينا روماني فوزي", 30,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Mohamed Abo El-Hagag", 31,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Amera Abdelaziz Saber", 32,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Ammar Yasser farouk", 33,  "%", 15, "silver");
+addStudentBorder("img/user.png", "كيرلس مجدي فايز", 34,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Dalia Abd Elazim", 35,  "%", 15, "silver");
+addStudentBorder("img/user.png", "مي عاطف شفيق", 36,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Doha Aly Mohammed", 37,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Yasser Muhamed Desokey", 38,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Saad Mohamed Hassan", 39,  "%", 15, "silver");
+addStudentBorder("img/user.png", "محمود عاطف محمد", 40,  "%", 15, "silver");
+addStudentBorder("img/user.png", "مهاب ابراهيم محمود", 41,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Bassant Hussein fathy", 42,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Adel Ahmed Mohamed", 43,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Zeyad aymen Mohamed", 44,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Bishoy Sedra Saber", 45,  "%", 15, "silver");
+addStudentBorder("img/user.png", "Mostafa Mohamed Bayoumi", 46,  "%", 15, "silver");
 //Bronze Students
 
 
